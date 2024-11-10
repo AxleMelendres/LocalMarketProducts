f24af58b8b58802d7dabcef6736fedb29642da6a
Ralph Lucero <ralphluceroxx@gmail.com>
2024-11-10 14:42:22 +0800

    First Commit

---
+++ vendorsprofile.js
@@ -0,0 +1,11 @@
+
+        const addProductButton = document.getElementById('add-products');
+        const editProductButton = document.getElementById('edit-products');
+
+        addProductButton.addEventListener('click', function() {
+            window.location.href = "add_product.html";
+        });
+
+        editProductButton.addEventListener('click', function() {
+            window.location.href = "edit_product.html"; 
+        });
