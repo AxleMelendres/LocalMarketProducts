f06d35312c69b7216f7bf3f219dc4061b2a2d3f0
Alex <alexrefuerzocapute@gmail.com>
2024-11-13 21:05:14 +0800

    registration and signup

--- login.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('customer-login').onclick = function() {
-    document.getElementById('login-form').style.display = 'block';
-    document.getElementById('login-title').textContent = 'Customer Login'; // Change title
-};
-document.getElementById('seller-login').onclick = function() {
-    document.getElementById('login-form').style.display = 'block';
-    document.getElementById('login-title').textContent = 'Seller Login'; // Change title
-};
---
+++ submit.php
@@ -0,0 +1,47 @@
+<?php
+
+$hostname = 'localhost';
+$dbname = 'dbgroup1';
+$username = 'root';
+$password = '';
+
+$conn = new mysqli($hostname, $username, $password, $dbname);
+
+if($conn->connect_error){
+    die("Connection Failed" .$conn->connect_error);
+} 
+
+if($_SERVER["REQUEST_METHOD"] == "POST")
+{
+
+    if (!isset($_POST['terms']) || $_POST['terms'] !== 'accepted') {
+        die("You must agree that the information you provided are valid and correct.");
+    }
+
+    $name = $_POST['name'];
+    $uname = $_POST['username'];
+    $email = $_POST['email'];
+    $contact = $_POST['contact'] ?? '';
+    $password = $_POST['password'];
+    $purpose = $_POST['purpose'];
+
+    $hashedPassword = password_hash($password, PASSWORD_DEFAULT);//add
+
+    $txt = $conn->prepare("INSERT INTO account (`Full Name`, Username, Email, `Contact Number`, Password, Purpose) VALUES (?, ?, ?, ?, ?, ?)");
+   
+    if ($txt === false) {
+        die("Prepare failed: " . $conn->error);
+    }
+    $txt->bind_param("ssssss", $name, $uname, $email, $contact, $hashedPassword, $purpose);
+
+    if ($txt->execute()) {
+         echo "Record created successfully"; 
+        }else {
+         echo "Error: " . $txt->error;
+        }
+        $txt->close();
+}
+
+$conn->close();
+
+?>
