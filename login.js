e6aaa74f2d3af4cebc282856d16bd74a16cfd000
Ax <151546135+AxleMelendres@users.noreply.github.com>
2024-11-14 22:08:28 +0800

    Delete login.js

--- login.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('customer-login').onclick = function() {
-    document.getElementById('login-form').style.display = 'block';
-    document.getElementById('login-title').textContent = 'Customer Login';
-};
-
-document.getElementById('seller-login').onclick = function() {
-    document.getElementById('login-form').style.display = 'block';
-    document.getElementById('login-title').textContent = 'Seller Login';
-};
