bf896df4f4996c145ea8729ff36094c58d3bf810
Ralph Lucero <ralphluceroxx@gmail.com>
2024-11-16 21:21:21 +0800

    Fixed connection

--- JS/vendorsprofile.js
+++ JS/vendorsprofile.js
@@ -1,18 +1,15 @@
-
-
 const addProductButton = document.getElementById('add-products');
 const editProductButton = document.getElementById('edit-products');
 const deleteProductButton = document.getElementById('delete-products');
 
 addProductButton.addEventListener('click', function() {
-    window.location.href = "add_product.html";
+    window.location.href = "../HTML/add_product.html";  
 });
 
 editProductButton.addEventListener('click', function() {
-    window.location.href = "edit_product.html"; 
+    window.location.href = "../HTML/edit_product.html";  
 });
 
 deleteProductButton.addEventListener('click', function() {
-    window.location.href = "delete_product.html"; 
+    window.location.href = "../HTML/delete_product.html"; 
 });
-
