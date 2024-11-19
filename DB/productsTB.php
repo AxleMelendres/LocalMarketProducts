542e976ea358bead708b0271502a46604bfaa1c0
Alex <alexrefuerzocapute@gmail.com>
2024-11-19 19:20:38 +0800

    edited

--- DB/productsTB.php
+++ DB/productsTB.php
@@ -9,18 +9,17 @@ class Product {
     public $product_quantity;
     public $product_price;
     public $product_description;
-    public $vendor_id;
+    public $product_category;
 
     public function __construct($db) {
         $this->conn = $db;
     }
 
     public function create() {
-        $query = "INSERT INTO products 
-                    (product_name, product_image, product_quantity, product_price, product_description, product_category, vendor_id)
-                  VALUES 
-                    (:product_name, :product_image, :product_quantity, :product_price, :product_description, :product_category, :vendor_id)";
-        $stmt = $this->conn->prepare($query);
+        $query = "INSERT INTO " . $this->tbl_name . " (product_name, product_image, product_quantity, product_price, product_description, product_category) 
+                  VALUES (:product_name, :product_image, :product_quantity, :product_price, :product_description, :product_category)";
+                          
+                  $stmt = $this->conn->prepare($query);
     
         $stmt->bindParam(':product_name', $this->product_name);
         $stmt->bindParam(':product_image', $this->product_image);
@@ -28,9 +27,13 @@ class Product {
         $stmt->bindParam(':product_price', $this->product_price);
         $stmt->bindParam(':product_description', $this->product_description);
         $stmt->bindParam(':product_category', $this->product_category);
-        $stmt->bindParam(':vendor_id', $this->vendor_id);
     
-        return $stmt->execute();
+        if ($stmt->execute()) {
+            return true;
+        } else {
+            echo "Error: " . $stmt->errorInfo()[2];
+            return false;
+        }
     }
     
 
